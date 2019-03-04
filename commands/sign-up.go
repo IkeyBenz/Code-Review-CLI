611d9c85069b260f01ed973c4b98fec1b5cd111f
Ikey Benzaken <ikey.benz@gmail.com>
2019-03-04 15:16:53 -0800

    [Completed] Register command function body

---
+++ commands/sign-up.go
@@ -0,0 +1,22 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/IkeyBenz/CodeReviewCLI/database"
+	"github.com/IkeyBenz/CodeReviewCLI/resources/users"
+	"github.com/spf13/cobra"
+)
+
+// SignUp will print the current time in a pretty way
+var SignUp = &cobra.Command{
+	Use:   "register",
+	Short: "Register for code review",
+	Long:  "Promps you for an email and a password and creates an account for you on code review.\n\tUsage:code-review register",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		defer database.MgoSession.Close()
+		err := users.NewUser(args[0], args[1])
+		fmt.Printf("You are now registered on code-review as %s\n", args[0])
+		return err
+	},
+}
