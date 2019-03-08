d3187ad581b458646d351503491c19c76961304a
Ikey Benzaken <ikey.benz@gmail.com>
2019-03-08 11:55:27 -0800

    Uncommented working code + removed future code

--- commands/make-request.go
+++ commands/make-request.go
@@ -1,10 +1,10 @@
 package commands
 
 import (
-	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"os"
-	"path"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -14,73 +14,72 @@ var MakeRequest = &cobra.Command{
 	Use: "request",
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		// formBody := GetFormBody(map[string]string{
-		// 	"answerer": "Enter recipient's email address: ",
-		// 	// You're going to have to get a different method to grab this as there will be \n characters in it
-		// 	"cr_request": "Enter your code in quotes: ",
-		// })
+		formBody := GetFormBody(map[string]string{
+			"answerer": "Enter recipient's email address: ",
+			// You're going to have to get a different method to grab this as there will be \n characters in it
+			"cr_request": "Enter your code in quotes: ",
+		})
 
-		// fmt.Println("Sending your request...")
+		fmt.Println("Sending your request...")
 
-		// bytesArr, err := json.Marshal(formBody)
-		// if err != nil {
-		// 	return err
-		// }
-
-		// req, err := http.NewRequest("POST", "https://code-review-api1.herokuapp.com/requests", bytes.NewBuffer(bytesArr))
-		// if err != nil {
-		// 	return err
-		// }
-
-		// token, err := ReadCookie()
-		// req.AddCookie(&http.Cookie{Name: "code-review", Value: token})
-		// req.Header.Set("Content-Type", "application/json")
-
-		// client := &http.Client{}
-		// resp, err := client.Do(req)
-		// if err != nil {
-		// 	return err
-		// }
-		// defer resp.Body.Close()
-
-		// var res map[string]interface{}
-
-		// json.NewDecoder(resp.Body).Decode(&res)
-
-		// fmt.Println(res)
-		// fmt.Println(res["data"])
-
-		// Ask for relative file path
-		filePath := GetInputs([]string{"Enter the relative path of the file containing your code: "})[0]
-		_, err := parseFileForCodeBlock(filePath)
+		bytesArr, err := json.Marshal(formBody)
 		if err != nil {
 			return err
 		}
+
+		req, err := http.NewRequest("POST", "https://code-review-api1.herokuapp.com/requests", bytes.NewBuffer(bytesArr))
+		if err != nil {
+			return err
+		}
+
+		token, err := ReadCookie()
+		req.AddCookie(&http.Cookie{Name: "code-review", Value: token})
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		var res map[string]interface{}
+
+		json.NewDecoder(resp.Body).Decode(&res)
+
+		fmt.Println(res)
+
+		// Ask for relative file path
+		// filePath := GetInputs([]string{"Enter the relative path of the file containing your code: "})[0]
+		// _, err := parseFileForCodeBlock(filePath)
+		// if err != nil {
+		// 	return err
+		// }
 		return nil
 
 	},
 }
 
-func parseFileForCodeBlock(relativePath string) (string, error) {
+// func parseFileForCodeBlock(relativePath string) (string, error) {
 
-	// Get file path
-	dir, _ := os.Getwd()
-	filePath := path.Join(dir, relativePath)
-	fmt.Println(filePath)
+// 	// Get file path
+// 	dir, _ := os.Getwd()
+// 	filePath := path.Join(dir, relativePath)
+// 	fmt.Println(filePath)
 
-	// Read file
-	file, err := os.Open(filePath)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
+// 	// Read file
+// 	file, err := os.Open(filePath)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		return "", err
-	}
-	return "", nil
-}
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		fmt.Println(scanner.Text())
+// 	}
+// 	if err := scanner.Err(); err != nil {
+// 		return "", err
+// 	}
+// 	return "", nil
+// }
